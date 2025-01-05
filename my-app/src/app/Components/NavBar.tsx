7fe0b4846bc4d96755a6268910d6d8c6144d32bf
jangyunhwan <jangyunhwan@gmail.com>
2025-01-06 01:51:34 +0900

    :art: Update tag link from a

--- my-app/src/app/Components/NavBar.tsx
+++ my-app/src/app/Components/NavBar.tsx
@@ -1,13 +1,53 @@
+import Link from "next/link";
+
 export default function NavBar() {
   return (
     <header className="lg:px-16 px-4 bg-white flex flex-wrap items-center py-4 shadow-md">
-      <div className="flex-1 flex justify-between items-center">
+      <Link
+        className="flex-1 flex justify-between items-center; text-xl"
+        href="/"
+      >
+        {" "}
+        Company{" "}
+      </Link>
+
+      <div
+        className="hidden md:flex md:items-center md:w-auto w-full"
+        id="menu"
+      >
+        <nav>
+          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
+            <li>
+              <Link className="md:p-4 py-3 px-0 block" href="/AboutUs">
+                AboutUs
+              </Link>
+            </li>
+            <li>
+              <Link className="md:p-4 py-3 px-0 block" href="#">
+                Treatments
+              </Link>
+            </li>
+            <li>
+              <Link className="md:p-4 py-3 px-0 block" href="#">
+                Blog
+              </Link>
+            </li>
+            <li>
+              <Link className="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      {/* <Link className="flex-1 flex justify-between items-center">
         <a href="/" className="text-xl">
           Company
         </a>
-      </div>
+      </Link> */}
 
-      <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
+      {/* <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
         <svg
           className="fill-current text-gray-900"
           xmlns="http://www.w3.org/2000/svg"
@@ -49,7 +89,7 @@ export default function NavBar() {
             </li>
           </ul>
         </nav>
-      </div>
+      </div> */}
     </header>
   );
 }
