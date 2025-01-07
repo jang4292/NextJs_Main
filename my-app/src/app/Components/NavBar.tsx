9c92451e0a195c9b3ec4363dfbf087bf15e025f2
JangYunHwan <jangyunhwan@gmail.com>
2025-01-07 14:17:38 +0900

    Remove unused codes

--- my-app/src/app/Components/NavBar.tsx
+++ my-app/src/app/Components/NavBar.tsx
@@ -40,56 +40,6 @@ export default function NavBar() {
           </ul>
         </nav>
       </div>
-
-      {/* <Link className="flex-1 flex justify-between items-center">
-        <a href="/" className="text-xl">
-          Company
-        </a>
-      </Link> */}
-
-      {/* <label htmlFor="menu-toggle" className="pointer-cursor md:hidden block">
-        <svg
-          className="fill-current text-gray-900"
-          xmlns="http://www.w3.org/2000/svg"
-          width="20"
-          height="20"
-          viewBox="0 0 20 20"
-        >
-          <title>menu</title>
-          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
-        </svg>
-      </label>
-      <input className="hidden" type="checkbox" id="menu-toggle" />
-
-      <div
-        className="hidden md:flex md:items-center md:w-auto w-full"
-        id="menu"
-      >
-        <nav>
-          <ul className="md:flex items-center justify-between text-base text-gray-700 pt-4 md:pt-0">
-            <li>
-              <a className="md:p-4 py-3 px-0 block" href="/AboutUs">
-                AboutUs
-              </a>
-            </li>
-            <li>
-              <a className="md:p-4 py-3 px-0 block" href="#">
-                Treatments
-              </a>
-            </li>
-            <li>
-              <a className="md:p-4 py-3 px-0 block" href="#">
-                Blog
-              </a>
-            </li>
-            <li>
-              <a className="md:p-4 py-3 px-0 block md:mb-0 mb-2" href="#">
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div> */}
     </header>
   );
 }
